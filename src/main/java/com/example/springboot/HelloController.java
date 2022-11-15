451342cdb8ae7e9fb5850186891b957898637f1a
Guy Maimon <guy@nextprint.co.il>
2022-11-15 13:42:24 +0200

    remove 123

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! - change from github-123";
+		return "Greetings from Spring Boot + Tanzu! - change from github";
 	}
 
 }
