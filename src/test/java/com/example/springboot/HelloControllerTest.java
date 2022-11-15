f4ba1a71b10b9dceeb1e08b6d2a62a649261abce
Guy Maimon <guy@nextprint.co.il>
2022-11-15 14:03:06 +0200

    add 123

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu! - change from github", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu! - change from github-123", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu! - change from github"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu! - change from github-123"));
     }
 }
